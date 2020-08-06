29dea8051c257e3ac0545ebd8728372bf9624e48
Tamás Gulácsi <tgulacsi78@gmail.com>
2020-08-06 07:41:26 +0200

    Rename grpctrace to gtrace

---
+++ gtrace/gtrace.go
@@ -0,0 +1,37 @@
+// Copyright 2020 Tamás Gulácsi
+//
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package gtrace
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel/api/kv"
+	"go.opentelemetry.io/otel/api/trace"
+	"go.opentelemetry.io/otel/instrumentation/grpctrace"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func Extract(ctx context.Context, metadata *metadata.MD, opts ...grpctrace.Option) ([]kv.KeyValue, trace.SpanContext) {
+	return grpctrace.Extract(ctx, metadata, opts...)
+}
+
+func Inject(ctx context.Context, metadata *metadata.MD, opts ...grpctrace.Option) {
+	grpctrace.Inject(ctx, metadata, opts...)
+}
+func StreamClientInterceptor(tracer trace.Tracer, opts ...grpctrace.Option) grpc.StreamClientInterceptor {
+	return grpctrace.StreamClientInterceptor(tracer, opts...)
+}
+func StreamServerInterceptor(tracer trace.Tracer, opts ...grpctrace.Option) grpc.StreamServerInterceptor {
+	return grpctrace.StreamServerInterceptor(tracer, opts...)
+}
+
+func UnaryClientInterceptor(tracer trace.Tracer, opts ...grpctrace.Option) grpc.UnaryClientInterceptor {
+	return grpctrace.UnaryClientInterceptor(tracer, opts...)
+}
+func UnaryServerInterceptor(tracer trace.Tracer, opts ...grpctrace.Option) grpc.UnaryServerInterceptor {
+	return grpctrace.UnaryServerInterceptor(tracer, opts...)
+}
