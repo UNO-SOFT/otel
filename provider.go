8bafabe11c644d0661c1ea270e6460b54c5ef3eb
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2024-03-15 12:04:10 +0100

    semconv/v1.24.0

--- provider.go
+++ provider.go
@@ -12,7 +12,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 )
 
 // https://opentelemetry.io/docs/instrumentation/go/getting-started/
