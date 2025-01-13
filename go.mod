7dc0ff6448a53a0bc7f72682bd0c1077378d3134
Tamás Gulácsi <T.Gulacsi@unosoft.hu>
2025-01-13 08:59:56 +0100

    SetupOTLP

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/UNO-SOFT/otel
 
-go 1.22.0
+go 1.22.7
 
 toolchain go1.23.4
 
@@ -11,13 +11,15 @@ require (
 	go.opentelemetry.io/otel/sdk v1.33.0
 	go.opentelemetry.io/otel/sdk/metric v1.33.0
 	go.opentelemetry.io/otel/trace v1.33.0
-	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
-	google.golang.org/grpc v1.65.0
+	google.golang.org/grpc v1.69.4
 )
 
 require (
+	go.opentelemetry.io/contrib/bridges/otelslog v0.8.0
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.9.0
 	go.opentelemetry.io/otel/exporters/stdout/stdoutmetric v1.33.0
 	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.33.0
 	go.opentelemetry.io/otel/log v0.9.0
@@ -25,13 +27,16 @@ require (
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.25.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/contrib/bridges/otelslog v0.8.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	go.opentelemetry.io/proto/otlp v1.5.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250106144421-5f5ef82da422 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250106144421-5f5ef82da422 // indirect
+	google.golang.org/protobuf v1.36.2 // indirect
 )
--- slog.go
+++ slog.go
@@ -6,9 +6,14 @@
 package otel
 
 import (
+	"context"
 	"log/slog"
+	"os"
+	"strings"
+	"time"
 
 	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
 )
 
 func NewSLogHandler(name string, lp *LoggerProvider, options ...otelslog.Option) slog.Handler {
@@ -28,3 +33,33 @@ func NewSLogger(name string, lp *LoggerProvider, options ...otelslog.Option) *sl
 func LogWithSchemaURL(schemaURL string) otelslog.Option { return otelslog.WithSchemaURL(schemaURL) }
 func LogWithSource(source bool) otelslog.Option         { return otelslog.WithSource(source) }
 func LogWithVersion(version string) otelslog.Option     { return otelslog.WithVersion(version) }
+
+// SetupOTLP returns an slog.Handler and a shutdown function,
+// iff OTEL_EXPORTER_OTLP_LOGS_ENDPOINT is specified.
+//
+// VL_ACCOUNT_ID+VL_PROJECT_ID or VL_TENANT_ID is used for providing henaders (AccountID, ProjectID) for VictoriaLogs.
+func SetupOTLP(ctx context.Context, serviceName, version string) (slog.Handler, func(context.Context), error) {
+	logsURL := os.Getenv("OTEL_EXPORTER_OTLP_LOGS_ENDPOINT")
+	if logsURL == "" {
+		return nil, nil, nil
+	}
+	resource, err := NewResource(serviceName, version)
+	if err != nil {
+		return nil, nil, err
+	}
+	opts := []otlploghttp.Option{otlploghttp.WithEndpointURL(logsURL), nil, nil}[:1]
+	if acc, proj := os.Getenv("VL_ACCOUNT_ID"), os.Getenv("VL_PROJECT_ID"); acc != "" && proj != "" {
+		opts = append(opts, otlploghttp.WithHeaders(map[string]string{"AccountID": acc, "ProjectID": proj}))
+	} else if acc, proj, ok := strings.Cut(os.Getenv("VL_TENANT_ID"), ":"); ok && acc != "" && proj != "" {
+		opts = append(opts, otlploghttp.WithHeaders(map[string]string{"AccountID": acc, "ProjectID": proj}))
+	}
+	if os.Getenv("OTEL_EXPORTER_OTLP_TIMEOUT") == "" {
+		opts = append(opts, otlploghttp.WithTimeout(time.Hour))
+	}
+	logExporter, err := otlploghttp.New(ctx, opts...)
+	if err != nil {
+		return nil, nil, err
+	}
+	lp := NewLoggerProvider(logExporter, resource)
+	return NewSLogHandler(serviceName, lp), func(context.Context) { lp.Shutdown(ctx); logExporter.Shutdown(ctx) }, nil
+}
